ba4408fa59792b366a96bca0ed5e81c59a0c8435
Arya <aryashirgaonkar42@gmail.com>
2022-11-15 09:48:20 +0530

    favourite recipes

---
+++ lib/models/wishlistmodel.dart
@@ -0,0 +1,17 @@
+import 'dart:core';
+import 'dart:core';
+
+class WishlistModel{
+    late String wishlisttitle;
+   late int wishlistservings;
+   late String wishlistimage;
+   late int wishlisttime;
+
+   WishlistModel({
+      required this.wishlisttitle,
+      required this.wishlistservings,
+      required this.wishlistimage,
+      required this.wishlisttime,
+});
+
+}
